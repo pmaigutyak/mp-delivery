943a38d125ccbcb3e519ae46b2730a105505b5a1
Pasha Maigutyak <pmaigutyak@gmail.com>
2022-02-19 17:14:22 +0200

    Update delivery app.

--- delivery/__init__.py
+++ delivery/__init__.py
@@ -3,6 +3,19 @@ from django.apps import AppConfig
 from django.utils.translation import ugettext_lazy as _
 
 
+def setup_settings(settings, is_prod, **kwargs):
+
+    settings['DATABASES']['delivery'] = {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mp',
+        'USER': 'dev'
+    }
+
+    settings['DATABASE_ROUTERS'] = settings.get('DATABASE_ROUTERS', []) + [
+        'delivery.routers.DeliveryRouter'
+    ]
+
+
 class DeliveryAppConfig(AppConfig):
     name = 'delivery'
     verbose_name = _('Delivery')

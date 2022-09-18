e5d8ede21f4ce662e277963e63afe1d256b4a96c
Pasha Maigutyak <pmaigutyak@gmail.com>
2022-09-18 12:27:51 +0300

    Upgrade to django 4.

--- delivery/fields.py
+++ delivery/fields.py
@@ -1,7 +1,7 @@
 
 from django.conf import settings
 from django.forms import Field, ValidationError
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.template.loader import render_to_string
 
 from delivery.widgets import DeliveryFormFieldWidget
--- setup.py
+++ setup.py
@@ -5,19 +5,18 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     requires = f.read().splitlines()
 
-__version__ = '0.1.2'
-
+version = '1.0.0'
 url = 'https://github.com/pmaigutyak/mp-delivery'
 
 
 setup(
     name='django-mp-delivery',
-    version=__version__,
+    version=version,
     description='Django delivery app',
     author='Paul Maigutyak',
     author_email='pmaigutyak@gmail.com',
     url=url,
-    download_url='%s/archive/%s.tar.gz' % (url, __version__),
+    download_url='%s/archive/%s.tar.gz' % (url, version),
     packages=find_packages(),
     include_package_data=True,
     license='MIT',

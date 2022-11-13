8a1f89b14daf28bc685b54beab300bfa10644837
Pasha Maigutyak <pmaigutyak@gmail.com>
2022-11-13 16:06:36 +0200

    Update version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     requires = f.read().splitlines()
 
-version = '1.0.0'
+version = '1.0.1'
 url = 'https://github.com/pmaigutyak/mp-delivery'
 
 
